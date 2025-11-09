39e09a476b1a767f1e6c2b9bb89fb74b007284bc
sriram-0509 <srineebalsubramaniyan@gmail.com>
2025-11-09 22:41:13 +0530

    boot

---
+++ RestControl_RestfulApi_project-8/src/main/java/com/tnsif/restcontrol_restapi_project8/RestControlRestfulApiProject8Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.restcontrol_restapi_project8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControlRestfulApiProject8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControlRestfulApiProject8Application.class, args);
+	}
+
+}
---
+++ RestControl_RestfulApi_project-8/src/test/java/com/tnsif/restcontrol_restapi_project8/RestControlRestfulApiProject8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.restcontrol_restapi_project8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControlRestfulApiProject8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
