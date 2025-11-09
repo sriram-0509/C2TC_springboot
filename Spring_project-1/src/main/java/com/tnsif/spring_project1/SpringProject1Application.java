765c2a27429cd5f2b13a85cd7fafcdc2742da3dd
sriram-0509 <srineebalsubramaniyan@gmail.com>
2025-11-09 22:44:35 +0530

    boot

---
+++ Spring_project-1/src/main/java/com/tnsif/spring_project1/SpringProject1Application.java
@@ -0,0 +1,14 @@
+package com.tnsif.spring_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject1Application.class, args);
+		System.out.println("Hello Connections....!");
+	}
+
+}
