9e0a5082964e1d0f96d01fd69493e5aa302a3633
sriram-0509 <srineebalsubramaniyan@gmail.com>
2025-11-09 22:39:04 +0530

    boot

---
+++ DI_ioc_project-3/src/main/java/com/tnsif/do_ioc_project3/DiIocProject3Application.java
@@ -0,0 +1,22 @@
+package com.tnsif.do_ioc_project3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DiIocProject3Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext var = SpringApplication.run(DiIocProject3Application.class, args);
+		
+		Customer1 c1 = var.getBean(Customer1.class);
+		Customer2 c2 = var.getBean(Customer2.class);
+		Customer3 c3 = var.getBean(Customer3.class);
+		
+		c1.show();
+		c2.show();
+		c3.show();
+	}
+
+}
---
+++ DI_ioc_project-3/src/test/java/com/tnsif/do_ioc_project3/DiIocProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.do_ioc_project3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiIocProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
